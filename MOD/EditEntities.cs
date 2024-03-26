a51e28d286407b187e0fcb5642a16fb17a983ecb
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2024-03-26 23:35:11 +0100

    UI and start game surface

---
+++ MOD/EditEntities.cs
@@ -0,0 +1,56 @@
+﻿using Extra.Lib.Helper;
+using Extra.Lib;
+using Game.Prefabs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity.Collections;
+using Unity.Entities;
+using Game.Objects;
+
+namespace ExtraDetailingTools
+{
+    internal static class EditEntities
+    {
+        internal static void SetupEditEntities()
+        {
+            EntityQueryDesc entityQueryDesc = new()
+            {
+                All = [ComponentType.ReadOnly<SurfaceData>()],
+                None = [ComponentType.ReadOnly<PlaceholderObjectElement>()],
+                
+            };
+
+            ExtraLib.AddOnEditEnities(new(OnEditEntities, entityQueryDesc));
+        }
+
+        private static void OnEditEntities(NativeArray<Entity> entities)
+        {
+
+            foreach (Entity entity in entities)
+            {
+                if (ExtraLib.m_PrefabSystem.TryGetPrefab(entity, out SurfacePrefab prefab))
+                {
+
+                    var SurfaceUI = prefab.GetComponent<UIObject>();
+                    if (SurfaceUI == null)
+                    {
+                        SurfaceUI = prefab.AddComponent<UIObject>();
+                        SurfaceUI.active = true;
+                        SurfaceUI.m_IsDebugObject = false;
+                        SurfaceUI.m_Icon = "Media/Game/Icons/LotTool.svg";
+                        SurfaceUI.m_Priority = 1;
+                    }
+
+                    SurfaceUI.m_Group?.RemoveElement(entity);
+                    SurfaceUI.m_Group = PrefabsHelper.GetOrCreateUIAssetCategoryPrefab("Landscaping", "Surfaces", "Media/Game/Icons/LotTool.svg", "Terraforming");
+                    SurfaceUI.m_Group.AddElement(entity);
+
+                    ExtraLib.m_EntityManager.AddOrSetComponentData(entity, SurfaceUI.ToComponentData());
+                }
+            }
+        }
+    }
+}
---
+++ Mod.cs
@@ -0,0 +1,30 @@
+﻿using Colossal.Logging;
+using Extra.Lib.Debugger;
+using Game;
+using Game.Modding;
+using Game.SceneFlow;
+
+namespace ExtraDetailingTools
+{
+    public class Mod : IMod
+    {
+        private static readonly ILog log = LogManager.GetLogger($"{nameof(ExtraDetailingTools)}.{nameof(Mod)}").SetShowsErrorsInUI(false);
+        internal static Logger Logger { get; private set; } = new(log);
+        public void OnLoad(UpdateSystem updateSystem)
+        {
+            
+            Logger.Info(nameof(OnLoad));
+
+            if (GameManager.instance.modManager.TryGetExecutableAsset(this, out var asset))
+                Logger.Info($"Current mod asset at {asset.path}");
+
+            EditEntities.SetupEditEntities();
+
+        }
+
+        public void OnDispose()
+        {
+            Logger.Info(nameof(OnDispose));
+        }
+    }
+}
