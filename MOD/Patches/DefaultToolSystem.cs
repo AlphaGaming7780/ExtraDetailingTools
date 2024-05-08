eec3f1b367a5d08808b095059d6708fd05d187d3
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-05-08 22:15:26 +0200

    done

--- MOD/Patches/DefaultToolSystem.cs
+++ MOD/Patches/DefaultToolSystem.cs
@@ -1,28 +1,19 @@
 ﻿using Game.Common;
 using Game.Tools;
-using Game;
 using HarmonyLib;
-using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Linq;
-using System.Reflection;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace ExtraDetailingTools.Patches
+namespace ExtraDetailingTools.Patches;
+
+internal class DefaultToolSystemPatche
 {
-    internal class DefaultToolSystemPatche
-    {
 
-        [HarmonyPatch(typeof(DefaultToolSystem), "InitializeRaycast")]
-        public class DefaultToolSystem_InitializeRaycast
-        {
-            public static void Postfix(DefaultToolSystem __instance)
-            {
-                ToolRaycastSystem toolRaycastSystem = Traverse.Create(__instance).Field("m_ToolRaycastSystem").GetValue<ToolRaycastSystem>();
-                toolRaycastSystem.raycastFlags |= RaycastFlags.EditorContainers;
-            }
-        }
-    }
+	[HarmonyPatch(typeof(DefaultToolSystem), "InitializeRaycast")]
+	public class DefaultToolSystem_InitializeRaycast
+	{
+		public static void Postfix(DefaultToolSystem __instance)
+		{
+			ToolRaycastSystem toolRaycastSystem = Traverse.Create(__instance).Field("m_ToolRaycastSystem").GetValue<ToolRaycastSystem>();
+			toolRaycastSystem.raycastFlags |= RaycastFlags.EditorContainers;
+		}
+	}
 }
