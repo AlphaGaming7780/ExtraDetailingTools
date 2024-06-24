2a9e6287bac93e79f2e14095c8a21dcbc1e33412
Alpha Gaming <69048433+AlphaGaming7780@users.noreply.github.com>
2024-06-24 14:16:10 +0200

    Update Patches.cs

--- MOD/Patches/Patches.cs
+++ MOD/Patches/Patches.cs
@@ -16,10 +16,10 @@ public class GameModeExtensions_IsEditor
 		MethodBase caller = new StackFrame(2, false).GetMethod();
 		if(
 			(caller.DeclaringType == typeof(NetToolSystem) && caller.Name == "GetNetPrefab") // ||
-			//(caller.DeclaringType == typeof(ObjectToolSystem) && caller.Name == "GetObjectPrefab")
+			//(caller.DeclaringType == typeof(ObjectToolSystem) && caller.Name == "GetObjectPrefab") // STRANGE STUFF HAPPEN WHEN RELOACTE BUILDING EXTENSION SINCE 1.1.5f1 GAME VERSION.
 			//(caller.DeclaringType == typeof(DefaultToolSystem) && caller.Name == "InitializeRaycast")
-			) {
-                    __result = true;
+		) {
+			__result = true;
 		}
 	}
 }
