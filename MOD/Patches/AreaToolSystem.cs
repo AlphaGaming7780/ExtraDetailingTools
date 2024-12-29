8f709edc8d652abd5b017077443c69409c687321
AlphaGaming7780 <69048433+AlphaGaming7780@users.noreply.github.com>
2024-12-29 19:56:19 +0100

    Update AreaToolSystem.cs

--- MOD/Patches/AreaToolSystem.cs
+++ MOD/Patches/AreaToolSystem.cs
@@ -3,20 +3,21 @@ using Game.Prefabs;
 using Game.Tools;
 using HarmonyLib;
 
-namespace ExtraDetailingTools;
-
-class AreaToolSystemPatch
+namespace ExtraDetailingTools
 {
-	[HarmonyPatch(typeof(AreaToolSystem), nameof(AreaToolSystem.GetAvailableSnapMask),
-		new Type[] { typeof(AreaGeometryData), typeof(bool), typeof(Snap), typeof(Snap) },
-		new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out })]
-	class AreaToolSystem_GetAvailableSnapMask
-	{
-        private static void Postfix(AreaGeometryData prefabAreaData, bool editorMode, ref Snap onMask, ref Snap offMask)
-		{
+    class AreaToolSystemPatch
+    {
+        [HarmonyPatch(typeof(AreaToolSystem), nameof(AreaToolSystem.GetAvailableSnapMask),
+            new Type[] { typeof(AreaGeometryData), typeof(bool), typeof(Snap), typeof(Snap) },
+            new ArgumentType[] { ArgumentType.Normal, ArgumentType.Normal, ArgumentType.Out, ArgumentType.Out })]
+        class AreaToolSystem_GetAvailableSnapMask
+        {
+            private static void Postfix(AreaGeometryData prefabAreaData, bool editorMode, ref Snap onMask, ref Snap offMask)
+            {
 
-			onMask |= Snap.ExistingGeometry | Snap.StraightDirection | Snap.ContourLines;
-			offMask |= Snap.ExistingGeometry | Snap.StraightDirection | Snap.ContourLines;
+                onMask |= Snap.ExistingGeometry | Snap.StraightDirection | Snap.ContourLines;
+                offMask |= Snap.ExistingGeometry | Snap.StraightDirection | Snap.ContourLines;
+            }
         }
-	}
+    }
 }
