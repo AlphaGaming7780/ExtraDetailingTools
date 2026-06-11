86d6473e49f917744ee89ded6ff74f3b1da2c5d8
Triton Supreme <69048433+AlphaGaming7780@users.noreply.github.com>
2026-06-11 15:15:13 +0200

    Update hello-world.tsx

--- UI/src/mods/hello-world.tsx
+++ UI/src/mods/hello-world.tsx
@@ -2,6 +2,8 @@ import { registry } from "index";
 import { TransformExtraPanel } from "./TransformExtraPanel/TransformExtraPanel";
 
 export const HelloWorldComponent = () => {
+    
+    console.log("Registering TransformExtraPanel...");
     if(registry.registry.has("ExtraLib/ExtraPanels/ExtraPanelsRoot/ExtraPanelsRoot"))
         registry.extend("ExtraLib/ExtraPanels/ExtraPanelsRoot/ExtraPanelsRoot", "extraPanelsComponents", TransformExtraPanel)
     else
